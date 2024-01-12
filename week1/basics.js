7e50fb4ad97425851f425696cc08001ddba6ae2d
SValanukonda <sarathvalanukonda491@gmail.com>
2024-01-12 09:35:59 +0530

    added assignments of week1 js

---
+++ week1/basics.js
@@ -0,0 +1,16 @@
+console.log('hello');
+
+// js is a dynamic language ie variable van reassigned with different data type
+let num=5;
+num="five";
+console.log(num);
+
+// single threaded language so it only uses single core so increasing computing power wont make fast
+// let and var can reassigned for const you cant change
+var a =1;
+console.log(a);
+let b =1;
+b=2;
+console.log(b);
+
+console.log("value of a ="+a+" and value of b="+b);
